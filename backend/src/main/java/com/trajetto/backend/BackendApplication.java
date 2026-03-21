0bf4949e3273f3e78eba903e709d82b3c3a92491
Ana Cezanoski <juliacezanoski@gmail.com>
2026-03-20 22:39:59 -0300

    CRUD e JWT User

--- backend/src/main/java/com/trajetto/backend/BackendApplication.java
+++ backend/src/main/java/com/trajetto/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package com.trajetto;
+package com.trajetto.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TrajettoApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TrajettoApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
---
+++ backend/src/main/java/com/trajetto/backend/user/dto/UserDTO.java
@@ -0,0 +1,27 @@
+package com.trajetto.backend.user.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private String telephone;
+    private String email;
+    private String country;
+    private String password;
+    private boolean isAdmin;
+//    private String profilePicture;
+
+    public boolean getIsAdmin() {
+        return this.isAdmin;
+    }
+}
