7345dd78b551372989dbf38995957c3b9143c1e5
Kauane S. R. <kau@MacBook-Pro-de-Kauane.local>
2026-03-24 13:00:43 -0300

    feat: require location permission to get current location

--- Repo-Front/Trajetto/app/(tabs)/mapa.tsx
+++ Repo-Front/Trajetto/app/(tabs)/mapa.tsx
@@ -1,22 +1,59 @@
-import {View, StyleSheet} from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
+import { useEffect, useState } from 'react';
+
+type Region = {
+    latitude: number;
+    longitude: number;
+    latitudeDelta: number;
+    longitudeDelta: number;
+};
+
+const Mapa = () => {
+    const [region, setRegion] = useState<Region | null>(null);
+
+    useEffect(() => {
+        const getLocation = async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+
+            if (status !== 'granted') {
+                console.log('Permissão de localização negada');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+
+            setRegion({
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01,
+            });
+        }
+
+        getLocation();
+    }, [])
 
 
-export default function Mapa() {
     return (
         <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: -25.4284,
-          longitude: -49.2733,
-          latitudeDelta: 0.01,
-          longitudeDelta: 0.01,
-        }}
-      />
-    </View>
+            <MapView
+                style={styles.map}
+                region={
+                    region ?? {
+                        latitude: -14.2350,
+                        longitude: -51.9253,
+                        latitudeDelta: 10,
+                        longitudeDelta: 10,
+                    }
+                }
+            />
+        </View>
     );
 }
+
+export default Mapa
 
 
 const styles = StyleSheet.create({
