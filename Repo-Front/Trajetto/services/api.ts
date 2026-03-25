191fad924a7d9bccce0ca21d0b98d64bcf6627fc
Kauane S. R. <kau@MacBook-Pro-de-Kauane.local>
2026-03-25 12:48:46 -0300

    feat(back & front): gets route calculation and draws polyline on map

---
+++ Repo-Front/Trajetto/services/api.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+
+const ip = '192.168.100.236'; // IP do seu PC
+
+export const api = axios.create({
+    // Usar o IP do seu PC aqui (o celular nao roda no localhost com o backend)
+    baseURL: `http://${ip}:8081`,
+})
