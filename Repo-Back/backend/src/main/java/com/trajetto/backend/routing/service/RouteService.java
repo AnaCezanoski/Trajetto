e9bff24aa58357e1ef73c5ba9a42bd916ad6e20f
Kauane S. R. <kau@MacBook-Pro-de-Kauane.local>
2026-03-25 11:51:09 -0300

    feat(backend): adds route endpoint and backend

---
+++ Repo-Back/backend/src/main/java/com/trajetto/backend/routing/service/RouteService.java
@@ -0,0 +1,65 @@
+package com.trajetto.backend.routing.service;
+
+import com.trajetto.backend.routing.dto.CoordinateDTO;
+import com.trajetto.backend.routing.dto.RouteRequestDTO;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class RouteService {
+
+    @Value("${ors.api.key}")
+    private String apiKey;
+
+
+    public Map<String, Object> getRoute(RouteRequestDTO request) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://api.openrouteservice.org/v2/directions/foot-walking";
+
+        List<List<Double>> coordinates =  new ArrayList<>();
+
+        coordinates.add(List.of(request.origin.lng, request.origin.lat));
+
+        if (request.waypoints != null) {
+            for (CoordinateDTO wp : request.waypoints) {
+                coordinates.add(List.of(wp.lng, wp.lat));
+            }
+        }
+
+        coordinates.add(List.of(request.destination.lng, request.destination.lat));
+
+        Map<String, Object> body = new HashMap<>();
+
+        body.put("coordinates", coordinates);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", apiKey);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(body, headers);
+
+        ResponseEntity<Map> response = restTemplate.exchange(
+                url,
+                HttpMethod.POST,
+                entity,
+                Map.class
+        );
+
+        Map route = (Map) ((List) response.getBody().get("routes")).get(0);
+        Map summary = (Map) route.get("summary");
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("geometry", route.get("geometry"));
+        result.put("distance", summary.get("distance"));
+        result.put("duration", summary.get("duration"));
+
+        return result;
+    }
+}
